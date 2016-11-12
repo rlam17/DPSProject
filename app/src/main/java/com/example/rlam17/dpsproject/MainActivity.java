1f62f24007600c322ca5b0ebead46765847653b7
Raymond Lam <rlam17@myseneca.ca>
2016-11-12 12:08:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rlam17/dpsproject/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.rlam17.dpsproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdapterView.OnItemClickListener heroClickHandler = new AdapterView.OnItemClickListener(){
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            //get data from the view that was clicked
+            String data = parent.getItemAtPosition(position).toString();
+            //show a toast for now.. or do whatever!
+            Toast.makeText(view.getContext(), data, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] heroesTemp = getResources().getStringArray(R.array.heroesTemp);
+        ListView tempHeroes = (ListView)findViewById(R.id.heroSelect);
+        ArrayAdapter<String> heroList = new ArrayAdapter<String>(this, R.layout.hero_slot, heroesTemp);
+
+        tempHeroes.setAdapter(heroList);
+        tempHeroes.setOnItemClickListener(heroClickHandler);
+
+    }
+
+
+
+}
