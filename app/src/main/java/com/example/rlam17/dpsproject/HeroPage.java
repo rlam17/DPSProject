80ed79038c5239c1aa939cdef0247d000014bf42
rlam17 <raymondlam4@hotmail.com>
2016-11-28 13:28:36 -0500

    Moved downloading to main activity for less downloading.

--- app/src/main/java/com/example/rlam17/dpsproject/HeroPage.java
+++ app/src/main/java/com/example/rlam17/dpsproject/HeroPage.java
@@ -32,106 +32,29 @@ import java.util.concurrent.ExecutionException;
 /**
  * Created by rlam17 on 11/14/2016.
  *
- * Uses opencsv module to read csv
- * Uses apache commons for FTPclient
+ *
+ *
  */
 
 
 public class HeroPage extends AppCompatActivity {
 
-    private class DownloadFromMatrix extends AsyncTask<Void, String, ArrayList<String[]>>{
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-            System.out.println("Hold on to your nutsack, we're going in!");
-        }
-
-        @Override
-        protected ArrayList<String[]> doInBackground(Void... f_url){
-
-
-            ArrayList<String[]> temp = new ArrayList<>();
-
-
-            JSch jsch = new JSch();
-            Session session;
-            try{
-                session = jsch.getSession("rlam17", "matrix.senecac.on.ca", 22);
-                session.setConfig("StrictHostKeyChecking", "no");
-                session.setPassword("Ray998051575@");
-                session.connect();
-
-                Channel channel = session.openChannel("sftp");
-                channel.connect();
-                ChannelSftp sftpChannel = (ChannelSftp) channel;
-                InputStream is = sftpChannel.get("herodata.csv");
-
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-                reader.readLine();
-                String line;
-
-                try{
-                    while ((line = reader.readLine()) != null) {
-                        String[] tokens = line.split(",");
-                        temp.add(tokens);
-                    }
-
-                } catch(IOException e){
-                    System.err.println("Something went wrong with reading file");
-                    System.err.println(e);
-                }finally{
-                    reader.close();
-                }
-
-                is.close();
-
-                sftpChannel.exit();
-                session.disconnect();
-            }catch(Exception e){
-                System.err.println("Something went wrong with FTP");
-                System.err.println(e);
-            }
-
-            return temp;
-        }
-    }
-
-
-    public List<Integer> intTotalStats = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hero_page);
 
 
-        //grab entire spreadsheet
 
         Intent intent = getIntent();
-        int heroSlot = intent.getIntExtra("heroChosen",0);
-
-
-
-        ArrayList<String[]> heroDirectory = null;
-        try {
-            heroDirectory = new DownloadFromMatrix().execute().get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+        String[] heroListing = intent.getStringArrayExtra("heroChosen");
 
 
 
 
-        String[] heroListing = heroDirectory.get(heroSlot);
 
         for(int i = 0; i< heroListing.length; i++)
         System.out.println(heroListing[i]);
-
-
-        //FTPClient ftpClient = new FTPClient();
-
-
         // Table colums are:
         //Heroname lmb1	lmb1pc	lmb1ps	rmb	rmbpc	rmbps	shift	shiftps	e	epc	q	qps	health	armor	shield	lmb2	lmb2pc	lmb2ps
 
--- app/src/main/java/com/example/rlam17/dpsproject/MainActivity.java
+++ app/src/main/java/com/example/rlam17/dpsproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rlam17.dpsproject;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,8 +10,82 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by rlam17 on 11/14/2016.
+ *
+ * Uses jsch for FTPclient
+ */
+
+public class MainActivity extends AppCompatActivity {
+    static public ArrayList<String[]> heroDirectory = null;
+    private class DownloadFromMatrix extends AsyncTask<Void, String, ArrayList<String[]>> {
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            System.out.println("Hold on to your nutsack, we're going in!");
+        }
+
+        @Override
+        protected ArrayList<String[]> doInBackground(Void... f_url){
+
+
+            ArrayList<String[]> temp = new ArrayList<>();
+
+
+            JSch jsch = new JSch();
+            Session session;
+            try{
+                session = jsch.getSession("rlam17", "matrix.senecac.on.ca", 22);
+                session.setConfig("StrictHostKeyChecking", "no");
+                session.setPassword("Ray998051575@");
+                session.connect();
+
+                Channel channel = session.openChannel("sftp");
+                channel.connect();
+                ChannelSftp sftpChannel = (ChannelSftp) channel;
+                InputStream is = sftpChannel.get("herodata.csv");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                reader.readLine();
+                String line;
+
+                try{
+                    while ((line = reader.readLine()) != null) {
+                        String[] tokens = line.split(",");
+                        temp.add(tokens);
+                    }
+
+                } catch(IOException e){
+                    System.err.println("Something went wrong with reading file");
+                    System.err.println(e);
+                }finally{
+                    reader.close();
+                }
+
+                is.close();
+
+                sftpChannel.exit();
+                session.disconnect();
+            }catch(Exception e){
+                System.err.println("Something went wrong with FTP");
+                System.err.println(e);
+            }
+
+            return temp;
+        }
+    }
     private AdapterView.OnItemClickListener heroClickHandler = new AdapterView.OnItemClickListener(){
         @Override
         public void onItemClick(AdapterView parent, View view, int position, long id) {
@@ -18,8 +93,11 @@ public class MainActivity extends AppCompatActivity {
             //String data = parent.getItemAtPosition(position).toString();
             //show a toast for now.. or do whatever!
             //Toast.makeText(view.getContext(), data, Toast.LENGTH_SHORT).show();
+
+            String[] heroListing = heroDirectory.get(position);
+
             Intent hero_selected = new Intent(MainActivity.this, HeroPage.class);
-            hero_selected.putExtra("heroChosen", position);
+            hero_selected.putExtra("heroChosen", heroListing);
             startActivity(hero_selected);
         }
     };
@@ -28,6 +106,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            heroDirectory = new DownloadFromMatrix().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
 
         String[] heroesTemp = getResources().getStringArray(R.array.heroesTemp);
         ListView tempHeroes = (ListView)findViewById(R.id.heroSelect);
