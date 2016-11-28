127994b40023a070606a0da898edfcd0b343a00c
rlam17 <raymondlam4@hotmail.com>
2016-11-28 13:32:39 -0500

    Slight cleanup

--- app/src/main/java/com/example/rlam17/dpsproject/HeroPage.java
+++ app/src/main/java/com/example/rlam17/dpsproject/HeroPage.java
@@ -1,33 +1,10 @@
 package com.example.rlam17.dpsproject;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-
-
-
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by rlam17 on 11/14/2016.
@@ -53,8 +30,8 @@ public class HeroPage extends AppCompatActivity {
 
 
 
-        for(int i = 0; i< heroListing.length; i++)
-        System.out.println(heroListing[i]);
+        for(String i : heroListing)
+        System.out.println(i);
         // Table colums are:
         //Heroname lmb1	lmb1pc	lmb1ps	rmb	rmbpc	rmbps	shift	shiftps	e	epc	q	qps	health	armor	shield	lmb2	lmb2pc	lmb2ps
 
