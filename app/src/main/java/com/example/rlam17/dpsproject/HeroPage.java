bf0d1f163b1fef439a8b479730c02d49b5c35f12
rlam17 <raymondlam4@hotmail.com>
2016-11-28 14:09:42 -0500

    Full table functionality

--- app/src/main/java/com/example/rlam17/dpsproject/HeroPage.java
+++ app/src/main/java/com/example/rlam17/dpsproject/HeroPage.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 
 /**
@@ -31,12 +32,23 @@ public class HeroPage extends AppCompatActivity {
 
         setTitle(heroListing[0]);
 
-
-
         // Table columns are:
         //Heroname lmb1	lmb1pc	lmb1ps	rmb	rmbpc	rmbps	shift	shiftps	e	epc	q	qps	health	armor	shield	lmb2	lmb2pc	lmb2ps
 
+        TextView lmb = (TextView)findViewById(R.id.lmbValue);
+        lmb.setText(heroListing[1]);
 
+        TextView rmb = (TextView)findViewById(R.id.rmbValue);
+        rmb.setText(heroListing[4]);
+
+        TextView shift = (TextView)findViewById(R.id.shiftValue);
+        shift.setText(heroListing[7]);
+
+        TextView e = (TextView)findViewById(R.id.eValue);
+        e.setText(heroListing[9]);
+
+        TextView q = (TextView)findViewById(R.id.qValue);
+        q.setText(heroListing[11]);
     }
 
 }
