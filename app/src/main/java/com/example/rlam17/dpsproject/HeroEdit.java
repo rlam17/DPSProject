876d79c7a1e6805c7b25112086f33b5962ddcc42
rlam17 <raymondlam4@hotmail.com>
2016-11-29 14:40:28 -0500

    Hero edit cleaned a bit

--- app/src/main/java/com/example/rlam17/dpsproject/HeroEdit.java
+++ app/src/main/java/com/example/rlam17/dpsproject/HeroEdit.java
@@ -1,5 +1,6 @@
 package com.example.rlam17.dpsproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,6 +14,18 @@ public class HeroEdit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hero_edit);
+
+        Intent intent = getIntent();
+        final String[] heroListing = intent.getStringArrayExtra("heroChosen");
+
+        for(String i : heroListing)
+            System.out.println(i);
+
+        setTitle(heroListing[0] + " edit page");
+
+        // Table columns are:
+        //Heroname lmb1	lmb1pc	lmb1ps	rmb	rmbpc	rmbps	shift	shiftps	e	epc	q	qps	health	armor	shield	lmb2	lmb2pc	lmb2ps
+
     }
 
 }
