c1afd5d99a3072014f2d3534fc16738e8b654f2d
rlam17 <raymondlam4@hotmail.com>
2016-11-30 14:00:55 -0500

    It actually works now

--- app/src/main/java/com/example/rlam17/dpsproject/HeroEdit.java
+++ app/src/main/java/com/example/rlam17/dpsproject/HeroEdit.java
@@ -1,12 +1,26 @@
 package com.example.rlam17.dpsproject;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  *
@@ -18,6 +32,63 @@ import android.widget.Toast;
 
 public class HeroEdit extends AppCompatActivity {
 
+    private class WriteToCSV extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            System.out.println("Hold on to your nutsack, we're going in!");
+        }
+
+        @Override
+        protected String doInBackground(String... in){
+
+
+            //ArrayList<String[]> temp = new ArrayList<>(); //For return
+
+
+            JSch jsch = new JSch();
+            Session session;
+            try{
+
+
+
+                String s = in[0];
+
+
+
+                //Black magic
+                session = jsch.getSession("rlam17", "matrix.senecac.on.ca", 22);
+                session.setConfig("StrictHostKeyChecking", "no");
+                session.setPassword("Ray998051575@");
+                session.connect();
+
+                Channel channel = session.openChannel("sftp");
+                channel.connect();
+                ChannelSftp sftpChannel = (ChannelSftp) channel;
+
+                ChannelExec channelExec = (ChannelExec)session.openChannel("exec");
+
+                channelExec.setCommand(s);
+                channelExec.connect();
+
+
+
+                sftpChannel.exit();
+                channelExec.disconnect();
+                session.disconnect();
+            }catch(Exception e){
+                System.err.println("Something went wrong with writing");
+                System.err.println(e);
+            }
+
+            return "Finished!";
+        }
+    }
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -25,6 +96,11 @@ public class HeroEdit extends AppCompatActivity {
 
         Intent intent = getIntent();
         final String[] heroListing = intent.getStringArrayExtra("editThisHero");
+        final String[] heroInput = new String[heroListing.length];
+
+        for(int k = 0; k < heroInput.length; k++){
+            heroInput[k] = heroListing[k];
+        }
 
         //for(String i : heroListing) System.out.println(i);
 
@@ -84,21 +160,39 @@ public class HeroEdit extends AppCompatActivity {
                 } else {
                     // Table columns are:
                     //Heroname lmb1	lmb1pc	lmb1ps	rmb	rmbpc	rmbps	shift	shiftps	e	epc	q	qps	health	armor	shield	lmb2	lmb2pc	lmb2ps
-                    String input = heroListing[0];
-                    heroListing[13] = heroHealth.getText().toString();
-                    heroListing[14] = heroArmor.getText().toString();
-                    heroListing[15] = heroShield.getText().toString();
-                    heroListing[1] = heroLMB1.getText().toString();
-                    heroListing[16] = heroLMB2.getText().toString();
-                    heroListing[4] = heroRMB.getText().toString();
-                    heroListing[7] = heroShift.getText().toString();
-                    heroListing[9] = heroE.getText().toString();
-                    heroListing[11] = heroQ.getText().toString();
+                    String input = heroInput[0];
+                    heroInput[13] = heroHealth.getText().toString();
+                    heroInput[14] = heroArmor.getText().toString();
+                    heroInput[15] = heroShield.getText().toString();
+                    heroInput[1] = heroLMB1.getText().toString();
+                    heroInput[16] = heroLMB2.getText().toString();
+                    heroInput[4] = heroRMB.getText().toString();
+                    heroInput[7] = heroShift.getText().toString();
+                    heroInput[9] = heroE.getText().toString();
+                    heroInput[11] = heroQ.getText().toString();
 
-                    for(int i = 1; i < heroListing.length;i++){
-                        input = input + "," + heroListing[i];
+                    for(int i = 1; i < heroInput.length;i++){
+                        input = input + "," + heroInput[i];
                     }
+                    String input2 = heroListing[0];
+                    for(int j = 1; j < heroListing.length;j++){
+                        input2 = input2 + "," + heroListing[j];
+                    }
+
+
                     //System.out.println(input);
+
+                    String command;
+                    command = "sed -i -e 's/" + input2 + "/" + input + "/g' herodata.csv";
+
+                    try {
+                        String response = new WriteToCSV().execute(command).get();
+                        System.out.println(response);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
